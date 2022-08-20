3959acdc94c8bca9e7626393ce1e4175e4cbae9f
Alberto <aquintero88@gmail.com>
2022-08-20 09:57:12 +0200

    added flags

--- src/views/CountriesList.jsx
+++ src/views/CountriesList.jsx
@@ -9,7 +9,11 @@ export default function CountriesList () {
         <div>
             <h2>List</h2>      
             {countries.map(elem => {
-                return <p key={elem.alpha3Code}><Link to={`/list/${elem.alpha3Code}`}>{elem.name.common}</Link></p>
+                return( 
+                <>
+                <p key={elem.alpha3Code}><Link to={`/list/${elem.alpha3Code}`}>{elem.name.common}</Link></p>
+                <img src={`https://flagpedia.net/data/flags/icon/72x54/${elem.alpha2Code.toLowerCase()}.png`} />
+                </>)
             })}
             <Outlet />           
         </div>
